2199de554915409a42c7633acd41f21ca670e277
Alexey Bobkov <xxxtrihardxxx@gmail.com>
2023-03-05 00:30:54 +0200

    add basic frontend login page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { UserSignupPage } from './pages/UserSignupPage';
+import * as apiCalls from './api/apiCalls';
+
+const actions = {
+    postSignup: apiCalls.signup
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <UserSignupPage actions = {actions} />
   </React.StrictMode>
 );
 
