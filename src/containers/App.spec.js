8b65fb4e8f4cbd41bdca25b7594f619509942e39
= <dignitionn@gmail.com>
2023-03-08 00:29:46 +0200

    add top navigation bar

--- src/containers/App.spec.js
+++ src/containers/App.spec.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {MemoryRouter} from "react-router-dom";
-import {render} from "@testing-library/react";
+import {fireEvent, render} from "@testing-library/react";
 import App from "./App";
 
 describe('App', () => {
@@ -15,7 +15,7 @@ describe('App', () => {
     }
 
     it('displays home page when url is /', () => {
-        const { queryByTestId } = setup('/');
+        const {queryByTestId} = setup('/');
         expect(queryByTestId('homepage')).toBeInTheDocument();
 
     });
@@ -25,7 +25,7 @@ describe('App', () => {
         expect(header).toHaveTextContent('Login');
     });
     it('displays only LoginPage  when url is /login', () => {
-        const { queryByTestId } = setup('/login');
+        const {queryByTestId} = setup('/login');
         expect(queryByTestId('homepage')).not.toBeInTheDocument();
     });
     it('displays UserSignupPage when url is /signup', () => {
@@ -34,9 +34,48 @@ describe('App', () => {
         expect(header).toHaveTextContent('Sign Up');
     });
     it('displays userpage  when url is other than /, /login or /signup', () => {
-        const { queryByTestId } = setup('/user1');
+        const {queryByTestId} = setup('/user1');
         expect(queryByTestId('userpage')).toBeInTheDocument();
-
+    });
+    it('displays topBar when url is /', () => {
+        const {container} = setup('/');
+        const navigation = container.querySelector('nav');
+        expect(navigation).toBeInTheDocument();
+    });
+    it('displays topBar when url is /login', () => {
+        const {container} = setup('/login');
+        const navigation = container.querySelector('nav');
+        expect(navigation).toBeInTheDocument();
+    });
+    it('displays topBar when url is /signup', () => {
+        const {container} = setup('/signup');
+        const navigation = container.querySelector('nav');
+        expect(navigation).toBeInTheDocument();
+    });
+    it('displays topBar when url is /user1', () => {
+        const {container} = setup('/user1');
+        const navigation = container.querySelector('nav');
+        expect(navigation).toBeInTheDocument();
+    });
+    it('shows the UserSignupPage when clicking signup', () => {
+        const {container, queryByText} = setup('/');
+        const signupLink = queryByText('Sign Up');
+        fireEvent.click(signupLink);
+        const header = container.querySelector('h1');
+        expect(header).toHaveTextContent('Sign Up');
+    });
+    it('shows the LoginPage when clicking login', () => {
+        const {container, queryByText} = setup('/');
+        const loginLink = queryByText('Login');
+        fireEvent.click(loginLink);
+        const header = container.querySelector('h1');
+        expect(header).toHaveTextContent('Login');
+    });
+    it('shows the HomePage when clicking logo', () => {
+        const {container, queryByTestId} = setup('/login');
+        const logo = container.querySelector('img');
+        fireEvent.click(logo);
+        expect(queryByTestId('homepage')).toBeInTheDocument();
     });
 });
 
